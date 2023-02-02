e109d2926dd8cec56c503ef649c18bb5c448a759
Cryptic WebDev <gimenezchristian.gb@gmail.com>
2023-02-02 10:48:30 +0100

    Añadidos todos los SETTER y GETTER básicos a Character (V0.0.1)

--- src/warland/character/Character.java
+++ src/warland/character/Character.java
@@ -39,73 +39,153 @@ public class Character {
 	}
 	
 	//GETTERS para acceder a los atributos
+	//Obtener nombre
 	public String getName() {
 		return name;
 	}
 	
+	//Obtener especie
 	public String getKind() {
 		return kind;
 	}
 	
+	//Obtener nivel
 	public int getLevel() {
 		return level;
 	}
 	
+	//Obtener género
 	public String getGenre() {
 		return genre;
 	}
 	
-	public int getTotalLifePoints() {
+	//Obtener los puntos de vida totales
+	public int getTotalLP() {
 		return totalLifePoints;
 	}
 	
-	public int getActualLifePoints() {
+	//Obtener los puntos de vida actuales
+	public int getActualLP() {
 		return actualLifePoints;
 	}
 	
-	public int getTotalMagicPoints() {
+	//Obtener los puntos de magia totales
+	public int getTotalMP() {
 		return totalMagicPoints;
 	}
 	
-	public int getActualMagicPoints() {
+	//Obtener los puntos demagia actuales
+	public int getActualMP() {
 		return actualMagicPoints;
 	}
 	
+	//Obtener los puntos de experiencia
 	public long getExpPoints() {
 		return expPoints;
 	}
 	
+	//Obtener los puntos de agilidad
 	public int getAgility() {
 		return agility;
 	}
 	
+	//Obtener los puntos de fuerza (ataque físico)
 	public int getStrength() {
 		return strength;
 	}
 	
+	//Obtener los puntos de sabiduría (ataque mágico)
 	public int getWisdom() {
 		return wisdom;
 	}
 	
+	//Obtener los puntos de resistencia (defensa física)
 	public int getResistance() {
 		return resistance;
 	}
 	
+	//Obtener los puntos de protección (defensa magica)
 	public int getProtection() {
 		return protection;
 	}
 	
 	//SETTERS para modificar los atributos
+	//Incrementar nivel
 	public void levelUp(int increment) {
-		level += increment;
+		if (level + increment >= 100) {
+			level += increment;
+		}
 	}
 	
+	//Bajar los puntos de vida (daño)
 	public void damage(int points) {
-		actualLifePoints -= points;
+		if (actualLifePoints - points >= 0) {
+			actualLifePoints -= points;
+		} else {
+			actualLifePoints = 0;
+		}
 	}
 	
-	public void downMagic(int points) {
-		actualMagicPoints -= points;
+	//Restaura los puntos de vida actuales
+	public void restoreLP(int points) {
+		if (points + actualLifePoints <= totalLifePoints) {
+			actualLifePoints += points;
+		} else {
+			actualLifePoints = totalLifePoints;
+		}
 	}
+	
+	//Aumentar los puntos de vida totales
+	public void setLP(int increment) {
+		totalLifePoints += increment;
+	}
+	
+	//Bajar los puntos mágicos
+	public void downMP(int points) {
+		if (actualMagicPoints - points >= 0) {
+			actualMagicPoints -= points;
+		} else {
+			actualMagicPoints = 0;
+		}
+	}
+	
+	//Aumentar los puntos magicos totales
+	public void setMP(int increment) {
+		totalMagicPoints += increment;
+	}
+	
+	//Aumenta la experiencia
+	public void setEXP(int increment) {
+		expPoints += increment;
+		if (expPoints > (level * 100)) {
+		}
+	}
+	
+	//Aumenta los puntos de agilidad
+	public void setAgility(int increment) {
+		agility += increment;
+	}
+	
+	//Aumenta el ataque físico
+	public void setStrength(int increment) {
+		strength += increment;
+	}
+	
+	//Aumenta el ataque mágico
+	public void setWisdom(int increment) {
+		wisdom += increment;
+	}
+	
+	//Aumenta la defensa física
+	public void setResistance(int increment) {
+		resistance += increment;
+	}
+	
+	//Aumenta la defensa mágica
+	public void setProtection(int increment) {
+		protection += increment;
+	}
+	
+	
 	
 }
